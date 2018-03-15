52e122b0fd845e121713f1d1a00bb4b2168aa03b
Alan Pinhel <alan.pinhel@objective.com.br>
2018-03-15 15:27:13 -0300

    Passando props

---
+++ learning-props/src/app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+import React from 'react'
+import Title from './title'
+
+var App = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <Title name='Alan' />
+      </div>
+    )
+  }
+})
+
+export default App
