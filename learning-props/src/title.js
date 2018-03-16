105d25dc687dff18d7384097885deb67d1baf39e
Alan Pinhel <alan.pinhel@objective.com.br>
2018-03-16 13:45:35 -0300

    renderizando componentes com funções puras

--- learning-props/src/title.js
+++ learning-props/src/title.js
@@ -2,16 +2,25 @@
 
 import React from 'react'
 
-const Title = React.createClass({
-  getDefaultProps: function () {
-    return {name: 'Desconhecido', lastName: 'Sem sobrenome'}
-  },
+const Title = ({name, lastName}) => (
+  <h1>{`Olá ${name} ${lastName.first}${lastName.last}`}</h1>
+)
 
-  render: function () {
-    return (
-      <h1>Olá {this.props.name + '  ' + this.props.lastName.first + this.props.lastName.last}!</h1>
-    )
-  }
-})
+Title.defaultProps = {
+  name: 'Desconhecido',
+  lastName: 'Sem sobrenome'
+}
+
+// const Title = React.createClass({
+//   getDefaultProps: function () {
+//     return {name: 'Desconhecido', lastName: 'Sem sobrenome'}
+//   },
+
+//   render: function () {
+//     return (
+//       <h1>Olá {this.props.name + '  ' + this.props.lastName.first + this.props.lastName.last}!</h1>
+//     )
+//   }
+// })
 
 export default Title
