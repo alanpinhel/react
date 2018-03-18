e81dc63092b4e4e77c3ce2e46e66907b089da2b1
Alan Pinhel <alan.pinhel@objective.com.br>
2018-03-18 15:11:06 -0300

    form select

--- learning-forms/src/app.js
+++ learning-forms/src/app.js
@@ -6,18 +6,22 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      valor: 'valor inicial'
+      value: '1'
     }
   }
 
   render () {
     return (
       <form>
-        <input type='text' value={this.state.valor} onChange={(e) => {
+        <select value={this.state.value} onChange={(e) => {
           this.setState({
-            valor: e.target.value
+            value: e.target.value
           })
-        }} />
+        }}>
+          <option value='1'>Opção 1</option>
+          <option value='2'>Opção 2</option>
+          <option value='3'>Opção 3</option>
+        </select>
       </form>
     )
   }
