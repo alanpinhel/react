7c55f00273d2dd69b266fb48f3f4defa3a36ee27
Alan Pinhel <alan.pinhel@objective.com.br>
2018-03-28 16:12:48 -0300

    tdd exceções

--- jest-in-practice/src/map/map.js
+++ jest-in-practice/src/map/map.js
@@ -1,6 +1,14 @@
 'use strict'
 
 const map = (arr = [], func = item => item) => {
+  if (!Array.isArray(arr)) {
+    throw new TypeError('The first parameter should be an array')
+  }
+
+  if (typeof func !== 'function') {
+    throw new TypeError('The second parameter must be a function')
+  }
+
   let newArray = []
 
   arr.forEach((element, index) => {
