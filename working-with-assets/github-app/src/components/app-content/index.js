fa0282c192064d9107cd79efa208e94d7f2d7f6f
Alan Pinhel <alan.pinhel@objective.com.br>
2018-04-09 18:06:32 -0300

    Otimizando critical rendering path

--- working-with-assets/github-app/src/components/app-content/index.js
+++ working-with-assets/github-app/src/components/app-content/index.js
@@ -1,10 +1,11 @@
 'use strict'
 
 import React, { PropTypes } from 'react'
-import Search from './search'
-import UserInfo from './user-info'
-import Actions from './actions'
-import Repos from './repos'
+import Search from '../search'
+import UserInfo from '../user-info'
+import Actions from '../actions'
+import Repos from '../repos'
+import './app.css'
 
 const AppContent = ({ userinfo, repos, starred, isFetching, handleSearch, getRepos, getStarred }) => (
   <div className='app'>
--- working-with-assets/github-app/webpack.prod.config.js
+++ working-with-assets/github-app/webpack.prod.config.js
@@ -5,6 +5,8 @@ const webpack = require('webpack')
 const validate = require('webpack-validator')
 const HtmlPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const crp = new ExtractTextPlugin('crp.css')
+const styles = new ExtractTextPlugin('[name]-[hash].css')
 
 module.exports = validate({
   entry: path.join(__dirname, 'src', 'index'),
@@ -15,7 +17,8 @@ module.exports = validate({
   },
 
   plugins: [
-    new ExtractTextPlugin('[name]-[hash].css'),
+    crp,
+    styles,
 
     new webpack.DefinePlugin({
       'process.env': {
@@ -32,6 +35,7 @@ module.exports = validate({
 
     new HtmlPlugin({
       title: 'Github app',
+      inject: false,
       template: path.join(__dirname, 'src', 'html', 'template.html')
     })
   ],
@@ -51,9 +55,14 @@ module.exports = validate({
       loader: 'babel'
     }, {
       test: /\.css$/,
+      exclude: /node_modules|(search|style)\.css/,
+      include: /src/,
+      loader: styles.extract('style', 'css')
+    }, {
+      test: /(search|style)\.css$/,
       exclude: /node_modules/,
       include: /src/,
-      loader: ExtractTextPlugin.extract('style', 'css')
+      loader: crp.extract('style', 'css')
     }]
   }
 })
