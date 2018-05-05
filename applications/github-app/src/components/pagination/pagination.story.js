1b05be127f66662cfc26dfb762d1e32d01ed25e6
Alan Pinhel <alanpinhel@gmail.com>
2018-05-05 16:14:36 -0300

    paginação storybook

--- applications/github-app/src/components/pagination/pagination.story.js
+++ applications/github-app/src/components/pagination/pagination.story.js
@@ -7,5 +7,19 @@ import Pagination from '.'
 const stories = storiesOf('<Pagination />', module)
 
 stories.add('without props', () => (
-  <Pagination total={10} activePage={5} pageLink='http://mypage.com/page/%page%' />
+  <Pagination />
+))
+
+stories.add('with total and activePage', () => (
+  <Pagination total={10} activePage={5} />
+))
+
+stories.add('with pageLink', () => (
+  <Pagination total={3} activePage={1} pageLink='http://mypage.com/page/%page%' />
+))
+
+stories.add('with callback', () => (
+  <Pagination total={15} activePage={7} pageLink='http://mypage.com/page/%page%' onClick={(page) => {
+    window.alert(page)
+  }} />
 ))
