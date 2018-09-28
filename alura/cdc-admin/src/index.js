9efcbb3337e4222b34035e4087cd74cd5a9f97fe
Alan Pinhel <alanpinhel@gmail.com>
2018-09-28 16:58:12 -0300

    react Router e o suporte a navegação

--- alura/cdc-admin/src/index.js
+++ alura/cdc-admin/src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import AutorBox from './Autor';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  (<Router>
+    <App>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/autor" component={AutorBox} />
+        <Route path="/livro" />
+      </Switch>
+    </App>
+  </Router>),
+  document.getElementById('root')
+);
 registerServiceWorker();
