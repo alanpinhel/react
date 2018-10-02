a151bf5daac65addae3f28f990e4f6dd8ff6d82c
Alan Pinhel <alanpinhel@gmail.com>
2018-10-02 13:40:08 -0300

    adicionado async / await, centralizado paths do webpack

--- reactjs-ninja/workflow-reactjs/src/app.js
+++ reactjs-ninja/workflow-reactjs/src/app.js
@@ -1,14 +1,37 @@
 'use strict'
 
-import React from 'react'
+import React, { Component } from 'react'
 import Title from 'components/title'
 
 import './css/style.css'
 
-const App = () => (
-  <div>
-    <Title>My App</Title>
-  </div>
-)
+class App extends Component {
+  constructor () {
+    super()
+    this.state = { title: '...' }
+  }
+
+  getTitle () {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve('My app with async / await!')
+      }, 2000)
+    })
+  }
+
+  async componentDidMount () {
+    try {
+      this.setState({ title: await this.getTitle() })
+    } catch (error) { }
+  }
+
+  render () {
+    return (
+      <div>
+        <Title>{this.state.title}</Title>
+      </div>
+    )
+  }
+}
 
 export default App
