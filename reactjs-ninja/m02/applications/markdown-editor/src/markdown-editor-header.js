ba67e1f9153645834482b3aba73237f501941838
Alan Pinhel <alanpinhel@gmail.com>
2018-10-03 16:42:33 -0300

    feedback de salvamento autómatico

--- reactjs-ninja/m02/applications/markdown-editor/src/markdown-editor-header.js
+++ reactjs-ninja/m02/applications/markdown-editor/src/markdown-editor-header.js
@@ -2,14 +2,14 @@
 
 import React, { PropTypes } from 'react'
 
-const MarkdownEditorHeader = ({ onSave }) => (
+const MarkdownEditorHeader = ({ isSaving }) => (
   <header className='editor-header'>
-    <button className='save' onClick={onSave}>Salvar</button>
+    <p className='save-message'>{isSaving ? 'Salvando...' : 'Salvo!'}</p>
   </header>
 )
 
 MarkdownEditorHeader.propTypes = {
-  onSave: PropTypes.func.isRequired
+  isSaving: PropTypes.bool.isRequired
 }
 
 export default MarkdownEditorHeader
