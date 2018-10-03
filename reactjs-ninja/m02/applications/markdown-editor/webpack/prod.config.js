ed51dabf634ef2d1cc6c58154cd1ebec1b1080f6
Alan Pinhel <alanpinhel@gmail.com>
2018-10-03 11:29:12 -0300

    ordenando os scripts no HTML

--- reactjs-ninja/m02/applications/markdown-editor/webpack/prod.config.js
+++ reactjs-ninja/m02/applications/markdown-editor/webpack/prod.config.js
@@ -13,8 +13,6 @@ module.exports = {
   output: common.output,
 
   plugins: [
-    new BundleAnalyzerPlugin(),
-
     new CleanPlugin(['dist'], {
       root: common.paths.root
     }),
@@ -41,12 +39,22 @@ module.exports = {
       minChunks: ({ resource }) => /node_modules/.test(resource)
     }),
 
-    new HtmlPlugin(common.htmlPluginConfig),
+    new HtmlPlugin(Object.assign({}, common.htmlPluginConfig, {
+      minify: { collapseWhitespace: true },
+      chunksSortMode: (chunk1, chunk2) => {
+        const order = ['react-build', 'vendor', 'main']
+        const left = order.indexOf(chunk1.names[0])
+        const right = order.indexOf(chunk2.names[0])
+        return left - right
+      }
+    })),
 
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true
     })
-  ],
+  ].concat(
+    process.env.ANALYZER ? new BundleAnalyzerPlugin() : []
+  ),
 
   module: {
     rules: [
