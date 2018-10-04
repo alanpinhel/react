a0ead0e6da2d4a32a62b28df7bb1d0e15a94ff01
Alan Pinhel <alanpinhel@gmail.com>
2018-10-04 16:11:23 -0300

    estruturado entrata com título e conteúdo

--- reactjs-ninja/m02/applications/markdown-editor/src/app.js
+++ reactjs-ninja/m02/applications/markdown-editor/src/app.js
@@ -36,12 +36,16 @@ class App extends Component {
 
     this.handleSave = () => {
       if (this.state.isSaving) {
-        localStorage.setItem(this.state.id, this.state.value)
+        const newFile = {
+          title: 'Sem titulo',
+          content: this.state.value
+        }
+        localStorage.setItem(this.state.id, JSON.stringify(newFile))
         this.setState({
           isSaving: false,
           files: {
             ...this.state.files,
-            [this.state.id]: this.state.value
+            [this.state.id]: newFile
           }
         })
       }
@@ -67,7 +71,7 @@ class App extends Component {
     this.textareaRef = node => { this.textarea = node }
 
     this.handleOpenFile = fileId => () => {
-      this.setState({ value: this.state.files[fileId], id: fileId })
+      this.setState({ value: this.state.files[fileId].content, id: fileId })
     }
   }
 
@@ -76,7 +80,7 @@ class App extends Component {
     this.setState({
       files: files.reduce((acc, fileId) => ({
         ...acc,
-        [fileId]: localStorage.getItem(fileId)
+        [fileId]: JSON.parse(localStorage.getItem(fileId))
       }), {})
     })
   }
