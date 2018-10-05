c1aedf3e7dbf2e05f9bed9d8abd8afeabe536ec5
Alan Pinhel <alanpinhel@gmail.com>
2018-10-05 09:52:42 -0300

    exemplo purecomponent

---
+++ reactjs-ninja/m02/pure-component-example/src/components/title/title.story.js
@@ -0,0 +1,48 @@
+'use strict'
+
+import { storiesOf } from '@kadira/storybook'
+import React, { Component } from 'react'
+import Title from './index'
+
+const stories = storiesOf('Title', module)
+
+stories.add('without props', () => (
+  <Title>Main title</Title>
+))
+
+stories.add('Title with async/await', () => {
+  class App extends Component {
+    constructor () {
+      super()
+      this.state = { title: '...' }
+    }
+
+    getTitle () {
+      return new Promise((resolve) => {
+        this.timer = setTimeout(() => {
+          resolve('My app with async / await!')
+        }, 2000)
+      })
+    }
+
+    async componentDidMount () {
+      try {
+        this.setState({ title: await this.getTitle() })
+      } catch (error) { }
+    }
+
+    componentWillUnmount () {
+      clearTimeout(this.timer)
+    }
+
+    render () {
+      return (
+        <div>
+          <Title>{this.state.title}</Title>
+        </div>
+      )
+    }
+  }
+
+  return <App />
+})
