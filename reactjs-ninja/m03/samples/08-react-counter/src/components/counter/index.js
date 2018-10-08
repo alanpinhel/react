a90e27e6eec6cc6698f84bb1180afad27955faf0
Alan Pinhel <alanpinhel@gmail.com>
2018-10-08 13:17:47 -0300

    exemplo 08

---
+++ reactjs-ninja/m03/samples/08-react-counter/src/components/counter/index.js
@@ -0,0 +1,40 @@
+'use strict'
+
+import React, { PureComponent } from 'react'
+import Counter from './counter'
+import { connect } from 'react-redux'
+
+class CounterContainer extends PureComponent {
+  constructor () {
+    super()
+    this.state = {
+      counter: 0
+    }
+
+    this.increment = () => {
+      this.props.dispatch({ type: 'INCREMENT' })
+    }
+
+    this.decrement = () => {
+      this.props.dispatch({ type: 'DECREMENT' })
+    }
+  }
+
+  render () {
+    return (
+      <Counter
+        counter={this.props.counter}
+        increment={this.increment}
+        decrement={this.decrement}
+      />
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    counter: state
+  }
+}
+
+export default connect(mapStateToProps)(CounterContainer)
