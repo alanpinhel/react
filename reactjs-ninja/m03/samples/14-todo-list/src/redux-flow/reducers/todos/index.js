f154485722ea4d0cea546d68092bacc39bc9c10c
Alan Pinhel <alanpinhel@gmail.com>
2018-10-15 08:23:10 -0300

    adicionado testes toggle todo

--- reactjs-ninja/m03/samples/14-todo-list/src/redux-flow/reducers/todos/index.js
+++ reactjs-ninja/m03/samples/14-todo-list/src/redux-flow/reducers/todos/index.js
@@ -1,5 +1,30 @@
+import { ADD_TODO, TOGGLE_TODO } from './actions';
+
 'use strict'
 
-const todos = () => [{ id: 0, text: 'Hey', completed: false }]
+const initialState = []
+
+const todos = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat({
+        id: action.payload.id,
+        text: action.payload.text,
+        completed: false
+      })
+
+    case TOGGLE_TODO:
+      return state.map(todo => {
+        if (todo.id !== action.payload.id) {
+          return todo
+        }
+
+        return {
+          ...todo,
+          completed: !todo.completed
+        }
+      })
+  }
+}
 
 export default todos
